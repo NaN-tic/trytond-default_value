9206a03c9d465314515007840b544499da62b1d8
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:22:27 +0100

    Cleanup tests

--- tests/test_default_value.py
+++ tests/test_default_value.py
@@ -7,30 +7,20 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class DefaultValueTestCase(unittest.TestCase):
-    '''
-    Test Default Value module.
-    '''
+    'Test Default Value module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('default_value')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('default_value')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
